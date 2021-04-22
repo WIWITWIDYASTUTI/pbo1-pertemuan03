ff2ed3a7018861e397d0a280173f4fca8b1f835c
wiwit <wiwit.widyastuti78@gmail.com>
2021-04-22 11:49:10 +0800

    Operator Binary

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperatorBinary {
+    String nama = "Wiwit Widyastuti";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.92;
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
